2ffdfa1da458bc20ab4dbc77f1559946de52aa3b
SHIVAM GUPTA <SHIVAM GUPTA>
2016-09-10 13:54:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhinav/messaging/ClickListenerChatFirebase.java
@@ -0,0 +1,27 @@
+package com.example.abhinav.messaging;
+
+/**
+ * Created by ABHINAV on 17-07-2016.
+ */
+import android.view.View;
+
+/**
+ * Created by Alessandro Barreto on 27/06/2016.
+ */
+public interface ClickListenerChatFirebase {
+
+    /**
+     * Quando houver click na imagem do chat
+     * @param view
+     * @param position
+     */
+    void clickImageChat(View view, int position, String nameUser, String urlPhotoUser, String urlPhotoClick);
+
+    /**
+     * Quando houver click na imagem de mapa
+     * @param view
+     * @param position
+     */
+    void clickImageMapChat(View view, int position, String latitude, String longitude);
+
+}
